9f23b51288aa132ef9bd3adc5b9ba201a317b451
artёm rudoy <art.rudy@gmail.com>
2023-08-30 21:31:43 +0300

    first commit

---
+++ dist/script.js
@@ -0,0 +1,29 @@
+// 1. two players game:
+//    1.1. ask the name of first player;
+//    1.1.1. save the input as firstPlayerName;
+//    1.2. ask the name of the srecond playr;
+//    1.2.1. save the input as secondPlayerName;
+//    1.3. define who goes first;
+//    1.4. using while loop ask users to take marches from table;
+//    1.5. the user who have to take the last of matches loose.
+// 2. one player game 
+//    2. ask user to choose level of difficulty;
+//    2.1. if it's easy:
+//       2.1.1 inside while loop computer takes randome quantity of matches from 1 to 3;
+//       2.1.2. user can take from 1 to 3 matches;
+//       2.1.3 last who takes -- loose;
+//    2.2. -//- medium :
+//       2.2.1. inside while loop computer takes randome quantity of matches from 1 to 3;
+//       2.2.2. user can take from 1 to 3 matches;
+//       2.2.3. ????????????
+//    2.3. 
+function nimGame() {
+    var numberOfPlayers = prompt('How many players are playing?');
+    switch (numberOfPlayers) {
+        case '1':
+        case '2':
+    }
+    function twoPlayerGame() {
+        var first;
+    }
+}
